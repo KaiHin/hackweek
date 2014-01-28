59ef943f44b27789a189ede8c3920ef2af9abf35
Kai Hin <kaihin6@gmail.com>
2014-01-28 15:53:56 +0100

    initial android project

---
+++ src/com/kaihin/hackweek/moodActivity.java
@@ -0,0 +1,15 @@
+package com.kaihin.hackweek;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class moodActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mood);
+    }
+}
